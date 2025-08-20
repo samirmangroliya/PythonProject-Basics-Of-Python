58248f0b22886fd9a00a85e83f35abbaee971c80
samir mangroliya <sbmangroliya2012@gmail.com>
2025-08-20 10:28:36 +0530

    casting and data type conversation added

---
+++ src/com/samir/python/datatypes/Casting.py
@@ -0,0 +1,28 @@
+#integers
+x = int(1)
+y = int(2.5)
+z = int("3")
+
+print(x)
+print(y)
+print(z)
+print(x,y,z)
+print(x+y+z)
+
+#floats
+print("\nFLOAT\n")
+x = float(1)
+y = float(2.8)
+z = float("3.2")
+print(x)
+print(y)
+print(z)
+
+#floats
+print("\nString\n")
+x = str("str1")
+y = str(2.8)
+z = str("8j")
+print(x)
+print(y)
+print(z)
